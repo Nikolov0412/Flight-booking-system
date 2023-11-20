81e16d6a7f63ea64c0caa423c0dc59bc46e51cd7
Nikolay Nikolov <NNNikolov@nemetschek.bg>
2023-11-20 10:55:32 +0200

    Added visualization for visualizing all seats across all flights. TODO: Add implementation for updating seat booked status and visualization for selected flight.'

--- booking-interface/src/pages/Seats.tsx
+++ booking-interface/src/pages/Seats.tsx
@@ -1,12 +1,81 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavigationBar from "../components/Navigation";
 import Footer from "../components/Footer";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import axios from "axios";
 
-const Seats: React.FC = () => {return(
+const columns: GridColDef[] = [
+  {
+    field: "id",
+    headerName: "UUID",
+    flex: 1,
+  },
+  {
+    field: "Row",
+    headerName: "Row",
+    flex: 1,
+  },
+  {
+    field: "Col",
+    headerName: "Col",
+    flex: 1,
+  },
+  {
+    field: "IsBooked",
+    headerName: "Booked",
+    flex: 1,
+  },
+  {
+    field: "FlightSectionId",
+    headerName: "Flight Section ID",
+    flex: 1,
+  },
+  {
+    field: "FlightNumber",
+    headerName: "Flight Number",
+    flex: 1,
+  },
+];
+const Seats: React.FC = () => {
+  const [data, setData] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://127.0.0.1:3000/seats");
+        setData(response.data);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
     <div>
-        <NavigationBar/>
-        <Footer/>
+      <NavigationBar />
+      <DataGrid
+        sx={{ minHeight: "400px " }}
+        rows={data}
+        columns={columns}
+        loading={loading}
+        getRowHeight={() => "auto"}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 25,
+            },
+          },
+        }}
+        pageSizeOptions={[25]}
+        checkboxSelection
+        disableRowSelectionOnClick
+      />
+      <Footer />
     </div>
-)}
+  );
+};
 
 export default Seats;
